19baf402e93befcb3bc76463be8c230c807c1615
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2013-09-11 16:32:40 +0000

    Making sure there's one iteration at least (copied from http://www.kendyck.com/archives/2005/03/20/solution-to-sicp-exercise-17/)

--- src/Exercise1_7.hs
+++ src/Exercise1_7.hs
@@ -2,18 +2,18 @@ module Exercise1_7 where
 
 main                        = print $ square (sqrt' 0.000005)
 
-sqrt'                       = sqrtIter 1
+sqrt'                       = sqrtIter 1 0
 
-sqrtIter guess x
-    | goodEnough newGuess guess
+sqrtIter guess prevGuess x
+    | goodEnough guess prevGuess
                             = guess
-    | otherwise             = sqrtIter newGuess x where
+    | otherwise             = sqrtIter newGuess guess x where
     newGuess                = improve guess x
 
 improve guess x             = average guess (x / guess)
 
 average x y                 = (x + y) / 2
 
-goodEnough newGuess guess   = abs (newGuess / guess - 1) < 0.001
+goodEnough guess prevGuess  = abs (guess / prevGuess - 1) < 0.001
 
 square x                    = x * x
